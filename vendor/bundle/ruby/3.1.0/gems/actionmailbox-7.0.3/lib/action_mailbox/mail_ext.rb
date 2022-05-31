3da4c957f70c48b1348f6a1dd2e736cda0785d6a
lisandro seia <lisandroseia11@gmail.com>
2022-05-31 17:41:15 -0300

    Delete vendor/bundle directory

--- vendor/bundle/ruby/3.1.0/gems/actionmailbox-7.0.3/lib/action_mailbox/mail_ext.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require 'mail'
-
-# The hope is to upstream most of these basic additions to the Mail gem's Mail object. But until then, here they lay!
-Dir["#{__dir__}/mail_ext/*"].each do |path|
-  require "action_mailbox/mail_ext/#{File.basename(path)}"
-end
--- vendor/bundle/ruby/3.1.0/gems/actionmailbox-7.0.3/lib/action_mailbox/mail_ext/address_equality.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Mail
-  class Address
-    def ==(other)
-      other.is_a?(Mail::Address) && to_s == other.to_s
-    end
-  end
-end
