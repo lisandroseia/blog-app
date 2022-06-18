9cfd96676675a1d76415dce42ccc987fdbb2a663
ForHemer <foreste.hemerson@gmail.com>
2022-06-17 22:08:13 -0400

    Update authentication_controller.rb file

--- app/controllers/api/authentication_controller.rb
+++ app/controllers/api/authentication_controller.rb
@@ -1,1 +1,12 @@
-..
+class Api::AuthenticationController < ApplicationController
+  before_action :authorize_request, except: :login
+  include Response
+  def authenticate
+    auth_token = AuthenticateUser.new(auth_params[:email], auth_params[:password]).call
+    json_response({ token: auth_token })
+  end
+  private
+  def auth_params
+    params.permit(:email, :password)
+  end
+end
